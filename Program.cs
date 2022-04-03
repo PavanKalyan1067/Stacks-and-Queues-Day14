efdc24e363e24f7094a932cbc8c00d73482ba583
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-03 23:05:09 +0530

    Welcomr to Stacks and

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksandQueuesDay14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks and Queues!");
+        }
+    }
+}
